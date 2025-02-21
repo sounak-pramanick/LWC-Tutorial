2996265231a49e0c01bc1ff01efb7eaa0fe52440
Sounak Pramanick <sounakmsd7@gmail.com>
2025-02-22 00:15:11 +0530

    Memory Game in LWC - completed

--- force-app/main/default/lwc/memoryGameLwc/memoryGameLwc.js
+++ force-app/main/default/lwc/memoryGameLwc/memoryGameLwc.js
@@ -24,6 +24,12 @@ export default class MemoryGameLwc extends LightningElement {
         {id: 1, listClass: "card", type: "cube", icon: "fa fa-cube"}
     ];
 
+    openedCards = [];
+    matchedCards = [];
+    moves = 0;
+    totalTime = "00:00";
+    timerRef;
+
     renderedCallback() {
         if(this.isLibLoaded) return;
         loadStyle(this, fontawesome + '/fontawesome/css/font-awesome.min.css')
@@ -39,5 +45,89 @@ export default class MemoryGameLwc extends LightningElement {
     displayCard(event) {
         let currCard = event.target;
         currCard.classList.add("open", "show", "disabled");
+        this.openedCards.concat(currCard);
+        if(this.openedCards.length === 2) {
+            this.moves++;
+            if(this.openedCards[0].type === this.openedCards[1].type) {
+                this.matchedCards.concat(this.openedCards[0], this.openedCards[1]);
+                this.matched();
+            }
+            else {
+                this.unmatched();
+            }
+        }
+    }
+
+    matched() {
+        this.openedCards[0].classList.add("match", "disabled");
+        this.openedCards[1].classList.add("match", "disabled");
+        this.openedCards[0].classList.remove("show", "open");
+        this.openedCards[1].classList.remove("show", "open");
+        this.openedCards = [];
+        if(this.matchedCards.length === 16) {
+            window.clearInterval(this.timerRef);
+        }
+    }
+
+    unmatched() {
+        this.openedCards[0].classList.add("unmatched");
+        this.openedCards[1].classList.add("unmatched");
+        this.action('DISABLE');
+        setTimeout(() => {
+            this.openedCards[0].classList.remove("show", "open", "unmatched");
+            this.openedCards[1].classList.remove("show", "open", "unmatched");
+            this.action('ENABLE');
+            this.openedCards = [];
+        }, 1200);
+    }
+
+    action(action) {
+        let cards = this.template.querySelectorAll('.card');
+        Array.from(cards).forEach(item => {
+            if(action === 'ENABLE') {
+                let isMatch = item.classList.contains('match');
+                if(!isMatch) {
+                    item.classList.remove("disabled");
+                }
+            }
+            else if(action === 'DISABLE') {
+                item.classList.add("diabled");
+            }
+        });
+    }
+
+    timer() {
+        let startTime = new Date();
+        this.timerRef = setInterval(() => {
+            let diff = new Date().getTime() - startTime.getTime();
+            let d = Math.floor(diff / 1000);
+            let m = Math.floor(d % 3600 / 60);
+            let s = Math.floor(d % 3600 % 60);
+            let mDisplay = m > 0? m + (m === 1? "minute, " : "minutes, ") : "";
+            let sDisplay = s > 0? s + (s === 1? "second" : "seconds") : "";
+            this.totalTime = mDisplay + sDisplay;
+        }, 1000);
+    }
+
+    shuffle() {
+        this.openedCards = [];
+        this.matchedCards = [];
+        this.moves = 0;
+        this.totalTime = "00:00";
+        window.clearInterval(this.timerRef);
+        let cardElems = this.template.querySelectorAll('.card');
+        Array.from(cardElems).forEach(item => {
+            item.classList.remove("show", "open", "match", "disabled");
+        })
+        let array = [...this.cards];
+        let counter = array.length;
+        while(counter > 0) {
+            let index = Math.floor(Math.random() * counter);
+            counter--;
+            let temp = array[counter];
+            array[counter] = array[index];
+            array[index] = temp;
+        }
+        this.cards = [...array];
     }
 }
