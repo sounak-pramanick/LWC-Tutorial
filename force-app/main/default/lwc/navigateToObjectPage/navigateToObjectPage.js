4e154822b316ef71539976a8231efa3c18eb173e
Sounak Pramanick <sounakmsd7@gmail.com>
2025-02-26 23:57:46 +0530

    Navigate to Object Page

---
+++ force-app/main/default/lwc/navigateToObjectPage/navigateToObjectPage.js
@@ -0,0 +1,75 @@
+import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import { encodeDefaultFieldValues } from 'lightning/pageReferenceUtils';
+
+export default class NavigateToObjectPage extends NavigationMixin(LightningElement) {
+    navigateToNewRecord() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Contact',
+                actionName: 'new'
+            }
+        })
+    }
+
+    navigateToCaseRecord() {
+        const defaultValues = encodeDefaultFieldValues({
+            Type: 'Other',
+            Subject: 'Case created from button',
+            Description: 'New case has been created'
+        });
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Case',
+                actionName: 'new'
+            },
+            state: {
+                defaultFieldValues: defaultValues
+            }
+        });
+    }
+
+    navigateToNewRecordWithDefault() {
+        const defaultValues = encodeDefaultFieldValues({
+            FirstName: 'John',
+            LastName: 'Doe',
+            LeadSource: 'Other'
+        });
+        
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Contact',
+                actionName: 'new'
+            },
+            state: {
+                defaultFieldValues: defaultValues
+            }
+        });
+    }
+
+    navigateToListView() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Contact',
+                actionName: 'list'
+            },
+            state: {
+                filterName: 'Recent'
+            }
+        });
+    }
+
+    navigateToFiles() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'ContentDocument',
+                actionName: 'home'
+            }
+        });
+    }
+}
