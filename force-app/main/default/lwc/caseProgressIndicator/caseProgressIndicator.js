de436a19b90f33fc39e99a40ddee6b3f553db52d
Sounak Pramanick <sounakmsd7@gmail.com>
2026-06-04 23:53:52 +0530

    refactor - adding html and js logic for case progress indicator

--- force-app/main/default/lwc/caseProgressIndicator/caseProgressIndicator.js
+++ force-app/main/default/lwc/caseProgressIndicator/caseProgressIndicator.js
@@ -1,3 +1,45 @@
-import { LightningElement } from 'lwc';
+import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
+import { api, LightningElement, track, wire } from 'lwc';
+import CASE_OBJECT from '@salesforce/schema/Case';
+import CASE_STATUS from '@salesforce/schema/Case.Status';
+import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
 
-export default class CaseProgressIndicator extends LightningElement {}
+export default class CaseProgressIndicator extends LightningElement {
+    @track caseStatusOptions = [];
+    caseStatusValue = '';
+    @api recordId;
+
+    @wire(getObjectInfo, {
+        objectApiName: CASE_OBJECT
+    })
+    caseObjectInfo;
+
+    @wire(getPicklistValues, {
+        recordTypeId: '$caseObjectInfo.data.defaultRecordTypeId',
+        fieldApiName: CASE_STATUS
+    })
+    casePicklistMethod({data, error}) {
+        if(data) {
+            // console.log('data=' + JSON.stringify(data));
+            this.caseStatusOptions = data.values;
+        }
+        if(error) {
+            console.error(error);
+        }
+    }
+
+    @wire(getRecord, {
+        recordId: '$recordId',
+        fields: [CASE_STATUS]
+    })
+    caseRecordOutput({data, error}) {
+        if(data) {
+            console.log('data here= ' + JSON.stringify(data));
+            this.caseStatusValue = getFieldValue(data, CASE_STATUS);
+            console.log('caseStatusValue= ' + this.caseStatusValue);
+        }
+        if(error) {
+            console.error('ERROR= ' + error);
+        }
+    }
+}
